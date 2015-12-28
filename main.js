a3d0e7e716f0a2aa3ba4031ee1eefbae3ef09db3
Jillian Fetter <jillian.fetter@gmail.com>
2015-12-27 21:52:06 -0700

    board displays customizable sized  stack of uniformly sized and colored disks

--- main.js
+++ main.js
@@ -3,25 +3,54 @@
 $(document).ready(init)
 
 function init(){
-	$("#newGame").on("click", unHideDisks)
-	$(".disks").on("change", generateDisks);
+	$(".disks").on("change", setDifficulty);
+	$(".startGame").on("click", hideDiskSelect);
+	$(".quit").on("click", reset);
 }
 
-function unHideDisks () {
-	$("diskSelection").removeClass("invisible");
+function hideDiskSelect () {
+	event.preventDefault();
+	$(".disks , label, .startGame").addClass("invisible");
+	$(".quit").removeClass("invisible");
+	var difficulty = Number($("form").children(":checked").val()) - 2;
+	if (difficulty)
+	$(".difficulty").text("Difficulty: " + difficulty).css("color", "red");
 }
 
-function generateDisks(event){
-	// number of disks to use
+function setDifficulty(event){
 	var diskCount = Number($("form").children(":checked").val());
-	console.log(diskCount)
-	//makeDisk().repeat(diskCount)
+	generateDisks(diskCount);
 }
 
+function generateDisks(diskCount){
+	$("#s1").empty();
+	console.log("generating " + diskCount + " disks");
+	diskCount ++;
+	var diskRow = [];
+		//make diskCount disks of increasing in size upto the full width of their container (12)
+	for (var i =1 ; i < diskCount; i ++){
+		var $div = $('<div class="row gameDisk">');
+		// change to have blocks sized and stacked appropriately;
+		$div.addClass("col-sm-" + i*2 + "offset-" + 12 - (i*2) );
+		$div.attr("data-value", i );
+		// change to have css generate a random color
+		$div.css("background-color", "pink" );
+		$("#s1").append($div);
+		// I dont think I'll need the array; 
+		diskRow.push($div)
+		console.log(diskRow);
+		//$(".gameDisk").on("click", removeDisk);
+		//$(".tower").on("click", tryToPlaceDisk);
+	}
+}
 
-function makeDisk (diskCount) {
-	
-		//return makeDisk()
+//removeDisk;
+
+//tryToPlace;
+
+
+function reset(){
+	location.reload();
 }
 
 
