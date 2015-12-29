63c877bd66bab0580c56080fe8731cbfde567898
Jillian Fetter <jillian.fetter@gmail.com>
2015-12-28 22:35:08 -0700

    can remove disks on click

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function setDifficulty(event){
 
 function generateDisks(diskCount){
 	$("#s1").empty();
-	console.log("generating " + diskCount + " disks");
+console.log("generating " + diskCount + " disks");
 	diskCount ++;
 //make diskCount disks of increasing in size upto the full width of their container (12)
 	for (var i =1 ; i < diskCount; i ++){
@@ -36,15 +36,34 @@ function generateDisks(diskCount){
 		$div.css("background-color", "pink" );
 		$row.append($div);
 		$("#s1").append($row);
-
-		//$(".gameDisk").on("click", removeDisk);
-		//$(".tower").on("click", tryToPlaceDisk);
 	}
+	$(".tower").on("click", moveDisk);
+	
 }
 
-//removeDisk;
+function moveDisk (event) {
+  if ($(".quit").hasClass("invisible"))
+  	return
+	var $selectedTower = $(this)
+	var $selectedStack = $("div.stack");
+	var $selectedDisk = $selectedTower.find($selectedStack).children().first()
+$selectedDisk.detach();
+	//if anything on the page is currently detached
+	// if ($(this).find(".detached")){
+	// 	console.log($("detached", ".detached"))
+	// 	//place disk on top of the stack (if its value is smaller... or blink screen if its not smaller)
+	// } else {
+	//if there is no disks in the column clicked -- flash the screen	
+// $selectedDisk.addClass("detached");
+// console.log("THIS ONE IS DETACHED", $(".detached"))
+// $(".detached").detach()
+	//}
+}
 
-//tryToPlace;
+// function detach	(event){
+// 	console.log("detached" + $(this))
+// }
+
 
 
 function reset(){
