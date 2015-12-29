a7670ff1857971a11a5f11aef1abb7880aa12fbd
Jillian Fetter <jillian.fetter@gmail.com>
2015-12-29 14:05:55 -0700

    game forces you to play correctly, but does not yet check for win status

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 $(document).ready(init)
 
 var $detached; 
+var moves =0; 
 
 function init(){
 	$(".disks").on("change", setDifficulty);
@@ -13,11 +14,12 @@ function init(){
 
 function hideDiskSelect () {
 	event.preventDefault();
-	$(".disks , label, .startGame").addClass("invisible");
-	$(".quit").removeClass("invisible");
 	var difficulty = Number($("form").children(":checked").val()) - 2;
-	if (difficulty)
-	$(".difficulty").text("Difficulty: " + difficulty).css("color", "red");
+	if (difficulty){
+		$(".difficulty").text("Difficulty: " + difficulty ).css("color", "red");
+		$(".disks , label, .startGame").addClass("invisible");
+		$(".quit").removeClass("invisible");
+	}
 }
 
 function setDifficulty(event){
@@ -34,37 +36,44 @@ console.log("generating " + diskCount + " disks");
 		var $row = $('<div class="row">')
 		var $div = $('<div class="gameDisk">');
 		$div.addClass("col-sm-" + i*2 );
-		$div.attr("data-value", i );
+	//	$div.attr("data-value", i );
 // change to have css generate a random color
 		$div.css("background-color", "pink" );
+		$row.data("value", i)
 		$row.append($div);
 		$("#s1").append($row);
 	}
 }
 
 function moveDisk (event) {
+	$(".moves").text("Moves: " + moves)
 	var $selectedTower = $(this);
 	var $selectedStack = $selectedTower.find("div.stack");
 	var $selectedDisk = $selectedStack.children().first();
 // dont allow pieces to be moved until start game has been clicked
-  if ($(".quit").hasClass("invisible"))
-  	return
-	if ($(".disksSelection").hasClass("diskDetached")){
-		//if(Number($detached.attr("data-value")) < Number($selectedDisk.attr("data-value")) || !$selectedDisk){
-			$selectedStack.prepend($detached)
-			$(".diskDetached").removeClass("diskDetached")
-			return	
-	//	} else {
-			return flashScreen();
-		}
-	$(".disksSelection").addClass("diskDetached");
-	$detached = $selectedDisk
-	$detached.detach();
+	  if ($(".quit").hasClass("invisible"))
+	  	return
+		if ($(".disksSelection").hasClass("diskDetached")){
+				moves ++;
+			if(Number($detached.data("value")) < Number($selectedDisk.data("value")) || !$selectedDisk.data("value")){
+				$selectedStack.prepend($detached)
+				$(".diskDetached").removeClass("diskDetached")
+				return	
+			} else {
+				flashScreen();
+				return 
+			}
+	}
+		$(".disksSelection").addClass("diskDetached");
+		$detached = $selectedDisk;
+		$detached.detach();
 }
 
 function flashScreen(){
 	$("*").addClass("black");
+	setTimeout(function(){
 	$("*").removeClass("black");
+	},200);
 }
 
 function reset(){
